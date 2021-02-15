e6126eb02df7560ed3e7ce459e9f3adbe9a75023
javierTenza <78432859+JavierTenza@users.noreply.github.com>
2021-02-15 11:38:47 -0500

    logo principal

---
+++ app/src/main/java/com/reciclaje/Activity_inicio.java
@@ -0,0 +1,28 @@
+package com.reciclaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Activity_inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Activity_inicio.this,LoginActivity.class );
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 7000);
+    }
+}
